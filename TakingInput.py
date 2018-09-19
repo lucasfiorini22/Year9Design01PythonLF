c1140ec5aa9aca36dffdd611127322c3353e461d
Lucas Fiorini <lucas.fiorini@Y22-Design-Lucas-Fiorini.local>
2018-09-19 10:27:07 -0400

    TakingInput.py

---
+++ TakingInput.py
@@ -0,0 +1,14 @@
+import os
+
+#We used the input function to take an input
+#We have to have an assignment statement to store an input
+
+#input is a function. It runs a small program that
+#causes the computer to stop and wait for input
+fName = input("What is your first name: ")
+lName = input("WHat is your last name: ")
+
+print("Hi, "+fName+" "+lName)
+
+initialName = fName[0] + '.' + lName #adding strings is concatination 
+print("Hi, "+initialName)
